a593303fe6d209c7ce63931ad8b3934ea6cd14e3
Arilaon Santos <arilsonsantos@gmail.com>
2016-11-18 06:18:35 -0200

    Implementação simples de busca Resolved #49

--- src/main/java/br/com/orion/repository/ContaRepository.java
+++ src/main/java/br/com/orion/repository/ContaRepository.java
@@ -1,5 +1,7 @@
 package br.com.orion.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,6 @@ import br.com.orion.model.Conta;
 
 @Repository
 public interface ContaRepository extends JpaRepository<Conta, Long>{
-
+	
+	public List<Conta> findByNomeContaining(String nome);
 }
